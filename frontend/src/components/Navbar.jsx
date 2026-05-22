6a7a5eadfc6e8e3924d8e0dc67428d33425d6906
Ujjwal Kumar <123ujjwal.kr@gmail.com>
2026-05-22 20:34:08 +0530

    Made the Frontend Hero Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 import { NavLink, Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [visible, setVisible] = React.useState(false)
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      
+
       <img src={assets.logo} alt="logo" className='w-16' />
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
@@ -13,7 +16,7 @@ const Navbar = () => {
         <NavLink to='/' className='flex flex-col items-center gap-1'>
           <p>HOME</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
-        </NavLink> 
+        </NavLink>
 
         <NavLink to='/collection' className='flex flex-col items-center gap-1'>
           <p>COLLECTION</p>
@@ -33,27 +36,69 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-6'>
+
         <img src={assets.search_icon} className='w-5 cursor-pointer' />
-        
+
         <div className='group relative'>
-          <img src={assets.profile_icon} className='w-5 cursor-pointer' />  
-          <div className = 'group-hover:block hidden absolute  dropdown-menu right-0 pt-4' >
-            <div className = 'flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 rounded-lg shadow-lg' >
+
+          <img src={assets.profile_icon} className='w-5 cursor-pointer' />
+
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 rounded-lg shadow-lg'>
               <p className='cursor-pointer hover:text-black'>Orders</p>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
               <p className='cursor-pointer hover:text-black'>Logout</p>
+            </div>
 
-            </div> 
           </div>
+
         </div>
 
         <Link to='/cart' className='relative'>
+
           <img src={assets.cart_icon} className='w-5 cursor-pointer' />
-          <p className='absolute right-[-5px] -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>3</p>
+
+          <p className='absolute right-[-5px] -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>
+            3
+          </p>
+
         </Link>
 
-        <img src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' /> 
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className='w-5 cursor-pointer sm:hidden'
+        />
+
       </div>
+
+      {/* Sidebar menu for small screens */}
+
+      <div className={`absolute top-0 bottom-0 left-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+
+        <div className='flex flex-col text-gray-700'>
+
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+
+            <img
+              className='h-4 rotate-180'
+              src={assets.dropdown_icon}
+              alt=""
+            />
+
+            <p>Back</p>
+
+          </div>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 bor' to='/collection'>COLLECTION</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 bor' to='/about'>ABOUT</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 bor' to='/contact'>CONTACT</NavLink>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
